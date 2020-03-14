19ea747a097812c77cfcafb3bd805895c3767df2
Gucci <martiska.w@gmail.com>
2020-03-14 19:02:05 +0100

    added functionality to reverse and uppercase input string

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def main():
-    print("Hello World!")
+    print('Type some string to have it reversed and uppercase.\nType :q to exit.')
+    while True:
+        # get user input
+        txt = input("Your input string: ")
+
+        # program ending condition
+        if ':q' == txt:
+            break
+        # printing result in reversed order and in uppercase
+        print("Result", txt[::-1].upper())
 
 
 if __name__ == "__main__":
